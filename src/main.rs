563086cac346cee4c2ca7dff6b117ca3a00c9ca8
muimuifly <mumufly2001@163.com>
2024-09-26 19:54:18 -0400

    finish

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,9 @@ use sampler::State;
 fn main() {
     const N_STEP: usize = 1000;
     const N_DIM: usize = 2;
-    // Fill this
+    let mut state = State::<N_DIM>::new(0xdeadbeef);
+    for _ in 0..N_STEP {
+        state.take_step();
+        println!("{:?}, {:?}", state.arr[0], state.arr[1]);
+    }
 }
